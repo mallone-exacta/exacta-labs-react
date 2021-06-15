2f5413603b343bfd842e0755be88c3f48e3d294d
Mallone <mallone.nogueira@exactaworks.com.br>
2021-06-15 14:18:18 -0300

    create button component and create tasks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import { useState } from "react";
 import Input from "./components/Input";
+import Button from "./components/Button";
 
 function App() {
-  const [value, setValue] = useState("  ");
+  const [value, setValue] = useState("");
+  const [tasks, setTasks] = useState([]);
 
   function onChange(event) {
     setValue(event.target.value);
   }
 
-  return <div>{value && <Input value={value} onChange={onChange} />}</div>;
+  function onClick() {
+    setTasks([
+      ...tasks,
+      {
+        text: value,
+      },
+    ]);
+
+    setValue("");
+  }
+
+  return (
+    <div>
+      <form onSubmit={(event) => event.preventDefault()}>
+        <Input value={value} onChange={onChange} />
+
+        <Button onClick={onClick}>Adicionar task</Button>
+      </form>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Button/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+function Button({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
+}
+
+export default Button;
