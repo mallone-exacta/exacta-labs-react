9794ffb3b4be23453ed890ca1d3658ca2f9de09d
Mallone <mallone.nogueira@exactaworks.com.br>
2021-06-23 14:25:32 -0300

    create and remove tasks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useCallback, useState } from "react";
 
 import TaskForm from "../../components/TaskForm";
 import TaskList from "../../components/TaskList";
@@ -8,30 +8,31 @@ import * as S from "./styles";
 function Home() {
   const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {
-    Tasks.getTasks().then(setTasks);
+  const refreshTasks = useCallback(async () => {
+    const response = await Tasks.getTasks();
+
+    setTasks(response);
   }, []);
 
-  function addTask(value) {
-    setTasks([
-      ...tasks,
-      {
-        text: value,
-      },
-    ]);
+  async function createTask(text) {
+    await Tasks.createTask({ text });
+
+    refreshTasks();
   }
 
-  function removeTask(task) {
-    const index = tasks.indexOf(task);
+  async function removeTask(task) {
+    await Tasks.deleteTask(task.id);
 
-    const newTasks = [...tasks.slice(0, index), ...tasks.slice(index + 1)];
-
-    setTasks(newTasks);
+    refreshTasks();
   }
+
+  useEffect(() => {
+    refreshTasks();
+  }, [refreshTasks]);
 
   return (
     <S.Wrapper>
-      <TaskForm onSubmit={addTask} />
+      <TaskForm onSubmit={createTask} />
 
       <TaskList tasks={tasks} removeTask={removeTask} />
     </S.Wrapper>
--- src/services/tasks.js
+++ src/services/tasks.js
@@ -6,4 +6,16 @@ export class Tasks {
 
     return data;
   }
+
+  static async createTask(task) {
+    const { data } = await axios.post("http://localhost:3001/tasks", task);
+
+    return data;
+  }
+
+  static async deleteTask(id) {
+    const { data } = await axios.delete("http://localhost:3001/tasks/" + id);
+
+    return data;
+  }
 }
